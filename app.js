c528e940259b7763430c7a3b619f30c9189bec55
MZ17 <>
2021-05-27 14:02:06 +0000

    added html

---
+++ app.js
@@ -0,0 +1,19 @@
+
+var userName = 'user';
+
+var userName = prompt('Whats your name ?');
+alert("welcome to my badly stylized site " + userName)
+
+
+var favPhone = prompt('What is your smartphone?');
+
+if (favPhone === "iphone") {
+  document.write('<p> why? you like unnecessary expensive stuff.. sure.  </p>')
+} else if (favPhone === "samsung") {
+  document.write('<p> wrong!!! pixel is better </p>')
+} else if (favPhone === "mi") {
+  document.write('<p>like spywere hah? </p>')
+} else {
+  alert('why not? ')
+}
+
